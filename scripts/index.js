0fd1cf43eb209806a2e255b95d29fac4de6a9f0b
Rebecca Uranga <urangarebecca@gmail.com>
2019-03-05 10:55:30 -0500

    adds dictionary and translate function

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+console.log("Hello.");
+
+const leetDictionary = {
+// Keys are strings, just like in python
+// But in JS, quotes are optional
+    A: 4,
+    E: 3,
+    G: 6,
+    I: 1,
+    O: 0,
+    S: 5,
+    T: 7,
+};
+
+function translate(originalString) {
+    let result = '';
+    
+
+
+
+    return result;
+}
+
