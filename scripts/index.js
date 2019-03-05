616857cd498aa315c404d57f332472e417c870e2
Rebecca Uranga <urangarebecca@gmail.com>
2019-03-05 11:19:31 -0500

    moves leetDictionary into translate function

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,41 @@
 console.log("Hello.");
 
-const leetDictionary = {
-// Keys are strings, just like in python
-// But in JS, quotes are optional
-    A: 4,
-    E: 3,
-    G: 6,
-    I: 1,
-    O: 0,
-    S: 5,
-    T: 7,
-};
 
 function translate(originalString) {
+    const leetDictionary = {
+    // Keys are strings, just like in python
+    // But in JS, quotes are optional
+        A: "4",
+        E: "3",
+        G: "6",
+        I: "1",
+        O: "0",
+        S: "5",
+        T: "7",
+    };
+
     let result = '';
-    
 
+    // loop through each letter in originalString
+    // if it's in the dictionary, 
+    // put its value in `result`
 
+    for (let i = 0; i < originalString.length; i++) {
+        let theLetter = originalString[i].toUpperCase();
+        // console.log(theLetter);
+        // console.log(leetDictionary[theLetter]);
+        if (leetDictionary[theLetter]) {
+            result += leetDictionary[theLetter];
+        } else {
+            // if it's not just copy the letter into `result`
+            result += theLetter;
+        }
+    }
 
     return result;
+}
+
+const addressBook = {
+    
 }
 
